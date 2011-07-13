7157670a1d07865e7ac04b9f14e5325367818471
Jeffrey Drake <iaefai@me.com>
2011-07-12 23:50:51 -0400

    11 Tests for JDQueue pass

--- SampleTests/JDQueueTests.m
+++ SampleTests/JDQueueTests.m
@@ -180,24 +180,59 @@
     
     BOOL result = [q dequeue: n count: 0];
     
-    STAssertTrue(result &&          // result good
-                 (q.count == 10),    // size is 10
-                 @"result is %d (should be 1), size of queue is %d should be 10, elements are %f, %f, %f, %f, %f, %f, %f, %f, %f, %f should be 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ", 
-                 result, q.count, q.location[0], q.location[1], q.location[2], q.location[3], q.location[4], , q.location[5], q.location[6],
-                 q.location[7], q.location[8], q.location[9]); 
+    STAssertTrue(result && (q.count == 10), @"result is %d, size of queue is %d should be 10", result, q.count);
 }
 
 
 
 // Dequeue 1 From Empty           { } -> { } / { }	                               NO
-// Dequeue 1 From 1               { 1 } -> { } / { 1 }                             YES
+
+- (void) testDequeue1FromEmpty {
+    JDQueue *q = [[JDQueue alloc] initWithCapacity: 10];
+    
+    float n[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+//    [q enqueue: n count: 10];
+    
+    BOOL result = [q dequeue: n count: 1];
+    
+    STAssertTrue(!result && (q.count == 0), @"result is %d (should be 0), size of queue is %d should be 0", result, q.count);
+}
+
+
 // Dequeue 1 From 5               { 1 2 3 4 5 } -> { 2 3 4 5 } / { 1 }	           YES
-// Dequeue 1 From 10              { 1 2 3 4 5 6 7 8 9 10 } -> { 2 .. 10 } / { 1 }  YES
-// Dequeue 5 From Empty           { } -> { } / { }                                 NO
-// Dequeue 5 From 1               { 1 } -> { 1 } / { }                             NO
-// Dequeue 5 From 5               { 1 2 3 4 5 } -> { } / { 1 2 3 4 5 }             YES
+
+- (void) testDequeue1From5 {
+    JDQueue *q = [[JDQueue alloc] initWithCapacity: 10];
+    
+    float n[5] = { 1, 2, 3, 4, 5 };
+    
+    [q enqueue: n count: 5];
+    
+    float m[6] = { 0, 0, 0, 0, 0, 0 };
+    
+    BOOL result = [q dequeue: m count: 1];
+    
+    STAssertTrue(result && (q.count == 4) && m[0] == 1.0, @"result is %d (should be 0), size of queue is %d should be 4, dequeued a %f should be 1.0", result, q.count, m[0]);
+}
+
+
 // Dequeue 5 From 6               { 1 2 3 4 5 6 } -> { 6 } / { 1 2 3 4 5 }         YES
-// Dequeue 5 From 10              { 1 .. 10 } -> { 6 .. 10 } / { 1 .. 5 }          YES
+
+- (void) testDequeue5From6 {
+    JDQueue *q = [[JDQueue alloc] initWithCapacity: 10];
+    
+    float n[6] = { 1, 2, 3, 4, 5, 6 };
+    
+    [q enqueue: n count: 6];
+    
+    float m[6] = { 0, 0, 0, 0, 0, 0 };
+    
+    BOOL result = [q dequeue: m count: 5];
+    
+    STAssertTrue(result && (q.count == 1) && m[3] == 4.0, @"result is %d (should be 0), size of queue is %d should be 4, dequeued a %f should be 4.0 in fourth position", result, q.count, m[3]);
+}
+
 
 
 
